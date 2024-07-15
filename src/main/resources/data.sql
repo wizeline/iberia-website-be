b0c7ca19ac7ea1e5e29b4768efd29165c6bb61c2
santiagoise <santiagois@ext.inditex.com>
2024-07-15 13:52:56 +0200

    Added images links

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,22 @@
 INSERT INTO images (url, tag, destination) VALUES
-('https://shotstack-create-api-v1-assets.s3.amazonaws.com/xoypme01ob/01j2b-pbqs3-80g28-3ez7j-n6y6ee.png', 'family', 'France'),
-('https://shotstack-create-api-v1-assets.s3.amazonaws.com/xoypme01ob/01j2b-qhky5-tfvex-p6ymr-y9a8ry.png', 'family', 'Rome');
+('https://lh3.googleusercontent.com/d/1CgfdLoCMVUzortF4wDz2jjmVlbj3oCYu', 'family', 'Paris'),
+('https://lh3.googleusercontent.com/d/1xMrkkGGyAFCLSo_m6MXnk8BE0w50c7mX', 'family', 'Rome');
+('https://lh3.googleusercontent.com/d/1i70_yKVpCYHlhAKfiWhCuetMQJgofRSb', 'family', 'Amsterdam');
+('https://lh3.googleusercontent.com/d/1T_Paimn9exKYkda3y6oAVHQLze7v7N7n', 'family', 'Barcelona');
+('https://lh3.googleusercontent.com/d/1QnSN1UQr8iyGS19K2eCiBWVpyhHQueyL', 'family', 'London');
+('https://lh3.googleusercontent.com/d/1S3Q-UGmWpmsZUTqLLvtaoPsEXDmfCIXW', 'youth', 'Paris'),
+('https://lh3.googleusercontent.com/d/1s8SMeqvQ2WHHJ8Kq1UDXSxN8D7HB1O3L', 'youth', 'Rome');
+('https://lh3.googleusercontent.com/d/1yLq62vjuBUcGgj6BHBBrxGD0kpPCKHi9', 'youth', 'Amsterdam');
+('https://lh3.googleusercontent.com/d/1HY1jnEhfJSgVMO6Dz_bm4GEb2XMN3QQV', 'youth', 'Barcelona');
+('https://lh3.googleusercontent.com/d/1TiQC8hI02YH2BwG5CHw2UFXP1QA_YB-Z', 'youth', 'London');
+('https://lh3.googleusercontent.com/d/147he4uLrXyCbNl0INb3tHt2bzGLK9Yj3', 'business', 'Paris'),
+('https://lh3.googleusercontent.com/d/1IbW0iAsCODQMxLjwKewSVKj_YquB1w_S', 'business', 'Rome');
+('https://lh3.googleusercontent.com/d/1paE_Kbf-CPIY7vP-wKAU9eV0-lTH7GFT', 'business', 'Amsterdam');
+('https://lh3.googleusercontent.com/d/19kwaylANi5kELTIi8b2lVv7pgefcKdap', 'business', 'Barcelona');
+('https://lh3.googleusercontent.com/d/1dekhsY36uvz7DZghd3xsdG4-IX7q4nhZ', 'business', 'London');
 
 
 INSERT INTO videos (url, tag, destination) VALUES
-('https://www.videotest.com/test.mp4', 'family', 'France');
+('https://drive.google.com/uc?export=preview&id=19X2zw7qUej9xdewvU9EBQzX_2bZJ8kfG', 'family', 'France');
 
 
